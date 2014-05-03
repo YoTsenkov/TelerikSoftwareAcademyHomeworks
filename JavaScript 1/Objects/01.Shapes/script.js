c28236fbe04c61ec028db12e6101dcd0efa4d0af
YoTsenkov <yotsenkov@gmail.com>
2014-05-03 13:48:24 +0300

    objects hw without last task

---
+++ JavaScript 1/Objects/01.Shapes/script.js
@@ -0,0 +1,52 @@
+﻿function createPoint(x, y) {
+    return {
+        x: x,
+        y: y
+    }
+}
+
+function createLine(startPoint, endPoint) {
+    return {
+        x1: startPoint.x,
+        y1: startPoint.y,
+        x2: endPoint.x,
+        y2: endPoint.y,
+        length: function () {
+            var length = getDistance(startPoint, endPoint);
+            return length;
+        }
+    }
+}
+
+function getDistance(pointOne, pointTwo) {
+    var distance = Math.sqrt(Math.pow((pointOne.x - pointTwo.x), 2) + Math.pow((pointOne.y - pointTwo.y), 2));
+    return distance;
+}
+
+function canFormTriangle(lineOne, lineTwo, lineThree) {
+    if ((lineOne.length() + lineTwo.length()) > lineThree.length() &&
+        (lineOne.length() + lineThree.length()) > lineTwo.length() &&
+        (lineTwo.length() + lineThree.length()) > lineOne.length()) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//test getDistance
+var pointOne = createPoint(2, 5);
+var pointTwo = createPoint(5, 8);
+console.log(getDistance(pointOne, pointTwo));
+
+//test length()
+var lineOne = createLine(pointOne, pointTwo);
+console.log(lineOne.length());
+
+//test canFormTriangle
+var lineTwo = createLine(createPoint(5, 7), createPoint(6, 9));
+var lineThree = createLine(createPoint(6, 7), createPoint(3, 5));
+console.log(canFormTriangle(lineOne, lineTwo, lineThree));
+
+
+
---
+++ JavaScript 1/Objects/05.FindYoungestPerson/script.js
@@ -0,0 +1,21 @@
+﻿var persons = [
+  { firstname: "Much", lastname: "Script", age: 32 },
+  { firstname: "Wow", lastname: "Wow", age: 81 },
+  { firstname: "Such", lastname: "Code", age: 25 },
+  { firstname: "Dat", lastname: "Javascript", age: 3 }
+];
+
+function getYoungest(array) {
+    var youngest = array[0];
+    for (var index in array) {
+        if (array[index].age < youngest.age) {
+            youngest = array[index];
+        }
+    }
+
+    return youngest;
+}
+
+var youngest = getYoungest(persons);
+var fullName = youngest.firstname + ' ' + youngest.lastname;
+console.log(fullName);
