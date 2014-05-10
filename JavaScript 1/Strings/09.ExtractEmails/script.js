7efb080bb0a6621435ec484bd5a892c9b3997981
YoTsenkov <yotsenkov@gmail.com>
2014-05-10 12:01:35 +0300

    strings hw without 5.

---
+++ JavaScript 1/Strings/09.ExtractEmails/script.js
@@ -0,0 +1,23 @@
+﻿var text = 'ivan.petkanov@gmail.com twfdsf dfjg nas[d sami sdf spaidy@abv.bg dhfsvb gd fgui';
+
+function extractEmails(text) {
+    var buffText = text;
+    var regex = /\S*@\S*\.\S*/;
+    var emailArr = [];
+    while (true) {
+        var result = buffText.match(regex);
+        if (result === null) {
+            break;
+        }
+
+        emailArr.push(result[0]);
+        buffText = buffText.replace(regex, '');
+    }
+
+    return emailArr;
+}
+
+var emails = extractEmails(text);
+for (var index in emails) {
+    console.log(emails[index]);
+}
