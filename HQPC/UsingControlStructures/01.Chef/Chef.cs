9fee2ae83c92d271ea384707731118774308be88
Yordan Tsenkov <spaidyzkun@gmail.com>
2014-05-15 15:59:49 +0300

    Using Control Structs hw full

---
+++ HQPC/UsingControlStructures/01.Chef/Chef.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Chef
+{
+    public class Chef
+    {
+        private Bowl GetBowl()
+        {
+            //... 
+        }
+
+        private Carrot GetCarrot()
+        {
+            //...
+        }
+
+        private Potato GetPotato()
+        {
+            //...
+        }
+
+        private void Cut(Vegetable potato)
+        {
+            //...
+        }
+
+        public void Cook()
+        {
+            Bowl bowl = GetBowl();
+
+            Potato potato = GetPotato();
+            Peel(potato);
+            Cut(potato);
+            bowl.Add(potato);
+
+            Carrot carrot = GetCarrot();
+            Peel(carrot);
+            Cut(carrot);
+            bowl.Add(carrot);
+
+
+            //OR *depends on the specific case*
+
+
+            Potato potato = GetPotato();
+            Peel(potato);
+            Cut(potato);
+
+            Carrot carrot = GetCarrot();
+            Peel(carrot);
+            Cut(carrot);
+
+            Bowl bowl = GetBowl();
+            bowl.Add(potato);
+            bowl.Add(carrot);
+        }
+    }
+}
