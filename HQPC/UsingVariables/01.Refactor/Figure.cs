67121536437cca50b9550b0fbd04567db5f0e49f
Yordan Tsenkov <spaidyzkun@gmail.com>
2014-05-15 14:41:07 +0300

    HQPC Naming variables hw full

---
+++ HQPC/UsingVariables/01.Refactor/Figure.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Figure
+{
+    public double width;
+    public double height;
+    public Figure(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+
+    public static Figure RotateFigure(Figure figure, double figureAngle)
+    {
+        double newWidth = Math.Abs(Math.Cos(figureAngle)) * figure.width +
+            Math.Abs(Math.Sin(figureAngle)) * figure.height;
+        double newHeight = Math.Abs(Math.Sin(figureAngle)) * figure.width +
+            Math.Abs(Math.Cos(figureAngle)) * figure.height;
+        Figure newFigure = new Figure(newWidth, newHeight);
+
+        return newFigure;
+    }
+}
