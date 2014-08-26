143a9268ade7bf9ba244a543bbc9beea50a8bb04
YoTsenkov <yotsenkov@gmail.com>
2014-08-26 19:29:59 +0300

    Advanced DS without the trie search

---
+++ DS&A/AdvancedDSHW/2.FindRangeEfficiently/OrderedBagTest.cs
@@ -0,0 +1,45 @@
+﻿namespace _2.FindRangeEfficiently
+{
+    using System;
+    using Wintellect.PowerCollections;
+
+    public class OrderedBagTest
+    {
+        private static void PrintProductsInPriceRange(OrderedBag<Product> collection, int firstNproducts, decimal from, bool fromInclusive, decimal to, bool toInclusive)
+        {
+            var productsInRange = collection.Range(new Product("randomName", from), fromInclusive, new Product("randomName", to), toInclusive);
+            if (productsInRange.Count < firstNproducts)
+            {
+                firstNproducts = productsInRange.Count;
+            }
+
+            for (int i = 0; i < firstNproducts; i++)
+            {
+                Console.WriteLine(productsInRange[i].Name + " -> " + productsInRange[i].Price);
+            }
+        }
+
+        static void Main()
+        {
+            var products = new OrderedBag<Product>();
+
+            for (int i = 0; i < 500000; i++)
+            {
+                products.Add(new Product("name" + i, i));
+            }
+
+            Console.WriteLine("Enter starting price range:");
+            decimal startingPriceRange = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter ending price range:");
+            decimal endingPriceRange = decimal.Parse(Console.ReadLine());
+
+            //PrintProductsInPriceRange(products, 20, startingPriceRange, true, endingPriceRange, true);
+
+            //10 000 price searches test
+            for (int i = 0; i < 10000; i++)
+            {
+                PrintProductsInPriceRange(products, 20, startingPriceRange + i, true, endingPriceRange + i, true);
+            }
+        }
+    }
+}
---
+++ DS&A/AdvancedDSHW/2.FindRangeEfficiently/Product.cs
@@ -0,0 +1,49 @@
+﻿namespace _2.FindRangeEfficiently
+{
+    using System;
+
+    public class Product : IComparable<Product>
+    {
+        private string name;
+
+        public Product(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string  Name {
+            get
+            {
+                return this.name;
+            }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Product name cant be null or empty");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public decimal Price  { get; set; }
+
+        public int CompareTo(Product other)
+        {
+            if (this.Price < other.Price)
+            {
+                return -1;
+            }
+            else if (this.Price > other.Price)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
