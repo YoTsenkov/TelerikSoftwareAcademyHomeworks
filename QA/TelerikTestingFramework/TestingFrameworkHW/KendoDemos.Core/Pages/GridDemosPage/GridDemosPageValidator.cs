2e39234bc75f09949cae8c2a3169b4713bccb181
YoTsenkov <yotsenkov@gmail.com>
2014-10-15 12:24:25 +0300

    kendo task full

--- QA/TelerikTestingFramework/TestingFrameworkHW/KendoDemos.Core/Pages/GridDemosPage/GridDemosPageValidator.cs
+++ QA/TelerikTestingFramework/TestingFrameworkHW/KendoDemos.Core/Pages/GridDemosPage/GridDemosPageValidator.cs
@@ -1,6 +1,7 @@
 ﻿namespace KendoDemos.Core.Pages.GridDemosPage
 {
     using ArtOfTest.Common.UnitTesting;
+    using System;
 
     public class GridDemosPageValidator
     {
@@ -8,38 +9,68 @@
         {
             string firstValue = null;
             string secondValue = null;
-            switch (columnName)
+            int comparisonResult = 0;
+            int dataRowsCount = GridDemosPage.Instance.Map.CurrentGrid.DataItems.Count;
+
+            //if the grid is empty
+            if (dataRowsCount == 0)
             {
-                case GridColumnNames.CarMaker:
-                    firstValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[0][0].TextContent;
-                    secondValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[1][0].TextContent;
-                    break;
-                case GridColumnNames.CarModel:
-                    firstValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[0][1].TextContent;
-                    secondValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[1][1].TextContent;
-                    break;
-                case GridColumnNames.Year:
-                    firstValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[0][2].TextContent;
-                    secondValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[1][2].TextContent;
-                    break;
-                case GridColumnNames.Category:
-                    firstValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[0][3].TextContent;
-                    secondValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[1][3].TextContent;
-                    break;
-                case GridColumnNames.AirConditioner:
-                    firstValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[0][0].TextContent;
-                    secondValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[1][0].TextContent;
-                    break;
-                default:
-                    break;
+                throw new MissingMemberException("The grid is empty and the sorting can't be checked");
             }
 
-            int comparisonResult = firstValue.CompareTo(secondValue);            
+            //if theres only one item in the grid
+            if (dataRowsCount == 1)
+            {
+                Assert.IsTrue(true);
+                return;
+            }
+
+            for (int i = 0; i < dataRowsCount - 1; i++)
+            {
+                switch (columnName)
+                {
+                    case GridColumnNames.CarMaker:
+                        firstValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[i][0].TextContent;
+                        secondValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[i + 1][0].TextContent;
+                        break;
+                    case GridColumnNames.CarModel:
+                        firstValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[i][1].TextContent;
+                        secondValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[i + 1][1].TextContent;
+                        break;
+                    case GridColumnNames.Year:
+                        firstValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[i][2].TextContent;
+                        secondValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[i + 1][2].TextContent;
+                        break;
+                    case GridColumnNames.Category:
+                        firstValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[i][3].TextContent;
+                        secondValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[i + 1][3].TextContent;
+                        break;
+                    case GridColumnNames.AirConditioner:
+                        firstValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[i][4].TextContent;
+                        secondValue = GridDemosPage.Instance.Map.CurrentGrid.DataItems[i + 1][4].TextContent;
+                        break;
+                    default:
+                        break;
+                }
+
+                comparisonResult = firstValue.CompareTo(secondValue);
+                if (comparisonResult != 0)
+                {
+                    break;
+                }
+
+                //if all items are completely identical
+                if (i == dataRowsCount - 2 && comparisonResult == 0)
+                {
+                    Assert.IsTrue(true);
+                    return;
+                }
+            }                
 
             if (ascending)
             {
                 bool areAscending = false;
-                if (comparisonResult == -1 || comparisonResult == 0)
+                if (comparisonResult == -1)
                 {
                     areAscending = true;
                 }
@@ -49,7 +80,7 @@
             else
             {
                 bool areDescending = false;
-                if (comparisonResult == 1 || comparisonResult == 0)
+                if (comparisonResult == 1)
                 {
                     areDescending = true;
                 }
